d96254f337122d00e659c80bb7d16d56e3dbc5d3
dfSabogal <david.sabogal1@gmail.com>
2018-02-03 13:41:43 -0500

    clases y esquema de bd manejables desde admin django

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import *
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Categoria)
+admin.site.register(Promocion)
+admin.site.register(Comentario)
--- polls/models.py
+++ polls/models.py
@@ -4,3 +4,33 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    categoria=models.CharField(max_length=1000)
+
+class Usuario(models.Model):
+    nombre=models.CharField(max_length=1000)
+    apellido=models.CharField(max_length=1000)
+    pais=models.CharField(max_length=1000)
+    ciudad=models.CharField(max_length=1000)
+    direccion=models.CharField(max_length=1000)
+    correo=models.CharField(max_length=1000)
+    categorias = models.ManyToManyField(Categoria)
+
+class Promocion(models.Model):
+    nombre=models.CharField(max_length=1000)
+    imagen=models.CharField(max_length=1000)
+    descripcion = models.CharField(max_length=1000)
+    valor = models.IntegerField(default=0)
+    fecha = models.DateField
+    categoria=models.OneToOneField(Categoria,on_delete=models.CASCADE)
+
+class Comentario(models.Model):
+    comentario=models.CharField(max_length=1000)
+    promocion=models.ForeignKey(Promocion, on_delete=models.CASCADE)
+    usuario=models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+
+
+
+
+
