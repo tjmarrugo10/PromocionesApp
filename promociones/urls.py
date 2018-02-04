7680da5571020da789f429096421258a961f1689
Cristian <corpari25@gmail.com>
2018-02-03 23:15:32 -0500

    Test 1.2 deployment for the user interface

--- promociones/urls.py
+++ promociones/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from . import views
 
 from django.conf.urls import url
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
