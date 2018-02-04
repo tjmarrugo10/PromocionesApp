efff61498ed13eaa09f66ae7dad0df1f4a472039
Cristian <corpari25@gmail.com>
2018-02-03 23:11:03 -0500

    Test 1.2 deployment for the user interface

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
-
+from django.template import loader, Context
+from django.http import HttpResponse
 from .models import Promocion
 
 def index (request):
     list_promociones = Promocion.objects.all()
-    context = ('list_promociones': list_promociones)
-    return render(request, 'templates/index.html', context)
+    template = loader.get_template("index.html")
+    context = Context ({'list_promociones':list_promociones})
+
+    return HttpResponse(template.render(context))
+
 # Create your views here
