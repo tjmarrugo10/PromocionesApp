31b2549579ab35fd8f8e698debc2ccb28881e773
Cristian <corpari25@gmail.com>
2018-02-03 23:27:51 -0500

    Test 1.2 deployment for the user interface

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
-from django.template import loader, Context
-from django.http import HttpResponse
 from .models import Promocion
 
-def index (request):
-    list_promociones = Promocion.objects.all()
-    template = loader.get_template("index.html")
-    context = Context ({'list_promociones':list_promociones})
+def index(request):
 
-    return HttpResponse(template.render(context))
-
-# Create your views here
+    lista_promociones = Promocion.objects.all()
+    context = {'lista_promociones': lista_promociones}
+    return render(request, 'polls/index.html', context)
--- promociones/urls.py
+++ promociones/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from . import views
 
 from django.conf.urls import url
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index, name='index'),
 ]
